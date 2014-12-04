4f79ba59cf18cd6a4069dd531deba563112ea405
tdshap <tess.shapiro@yahoo.com>
2014-12-03 19:19:21 -0500

    intro page and nav bar complete with routes implemented

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+	
+})
+
 var WelcomePage = Backbone.View.extend({ 
 	tagName: "div", 
 	attributes: {
@@ -14,6 +18,29 @@ var WelcomePage = Backbone.View.extend({
 
 })
 
+var NavBar = Backbone.View.extend({
+	tagName: "nav",
+	attributes: {
+		class: "navigation"
+	},
+	template: _.template($("#navbar").html() ),
+	initialize: function(){
+		this.render()
+	},
+	render: function(){
+		var navbar = this.$el.html( this.template )
+		$(document.body).append(navbar)
+	}
+})
+
+
+var WorkView = Backbone.View.extend({
+	tagName: "div",
+	attributes: {
+		class: "work"
+	},
+
+})
 
 
 
@@ -21,14 +48,37 @@ var WelcomePage = Backbone.View.extend({
 var Router = Backbone.Router.extend({
 	routes: {
 		"": "home",
+		"work":"work",
+		"about":"about",
+		"skills":"skills",
+		"contact":"contact"
 	}
 })
 var router = new Router;
 
 router.on("route:home", function(){
+	$("div.container").empty()
+	navbar = new NavBar
 	welcomePage = new WelcomePage
-	console.log("home route")
-})
 
+
+})
+router.on("route:work", function(){
+	$("div.container").empty()
+
+})
+router.on("route:about", function(){
+	$("div.container").empty()
+
+})
+router.on("route:skills", function(){
+	$("div.container").empty()
+
+})
+router.on("route:contact", function(){
+	$("div.container").empty()
+
+
+})
 
 Backbone.history.start()
